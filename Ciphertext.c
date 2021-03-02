7e59163acd618373c6dbc04e022741a71cdc5933
ronney18 <Ronney_Sanchez@student.uml.edu>
2021-03-01 22:00:21 -0500

    Modified program and image

--- Ciphertext.c
+++ Ciphertext.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
-void storeAlphabet(char alphabet[]);
-void encryptAlphabet(char alphabet[], char encrypt[]);
-void decryptAlphabet(char encrypt[], char decrypt[]);
-void printAlphabet(char alphabet[]);
-void encryptMessage(char buffer[], int size);
-void decryptMessage(char description[], int size);
+void storeCharacters(char characters[]);
+void encryptCharacters(char characters[], char encrypt[]);
+void decryptCharacters(char encrypt[], char decrypt[]);
+void printCharacters(char characters[]);
+void encryptMessage(char* buffer, int size);
+void decryptMessage(char* description, int size);
 
 int main(int argc, char* argv[])
 {
@@ -17,134 +18,139 @@ int main(int argc, char* argv[])
 		return -1;
 	}
 
-	int size;
-	char read;
-	char alphabet[26];
-	char alphabetEncrypt[26];
-	char alphabetDecrypt[26];
-	FILE *in_file_encryption;
-	FILE *in_file_decryption;
-	FILE *out_file_encryption;
-	FILE *out_file_decryption;
-	in_file_encryption = fopen("huckleberry_finn.txt", "r");
+	else
+	{
+		int size;
+		char read;
+		char asciiCharacters[94];
+		char asciiEncrypt[94];
+		char asciiDecrypt[94];
+		FILE *in_file_encryption;
+		FILE *in_file_decryption;
+		FILE *out_file_encryption;
+		FILE *out_file_decryption;
+		in_file_encryption = fopen(argv[1], "r");
 
-	if(in_file_encryption == NULL)
-	{
-		puts("ERROR: File encryption cannot be opened!");
-		return -2;
-	}
-	fseek(in_file_encryption, 0, SEEK_END);
-	size = ftell(in_file_encryption);
-	rewind(in_file_encryption);
-	char buffer[size];
-	int i = 0;
-	while((read = fgetc(in_file_encryption)) != EOF)
-	{
-		buffer[i] = read;
-		i++;
-	}
-	fclose(in_file_encryption);
+		if(in_file_encryption == NULL)
+		{
+			perror("ERROR: File encryption cannot be opened!");
+			exit(EXIT_FAILURE);
+		}
+		fseek(in_file_encryption, 0, SEEK_END);
+		size = ftell(in_file_encryption);
+		rewind(in_file_encryption);
+		char buffer[size];
+		int i = 0;
+		while((read = fgetc(in_file_encryption)) != EOF)
+		{
+			*(buffer + i) = read;
+			i++;
+		}
+		fclose(in_file_encryption);
 	
-	storeAlphabet(alphabet);
-	encryptAlphabet(alphabet, alphabetEncrypt);
+		storeCharacters(asciiCharacters);
+		encryptCharacters(asciiCharacters, asciiEncrypt);
 
-	puts("------------Ciphertext Encryption------------");
-	printAlphabet(alphabet);
-	printAlphabet(alphabetEncrypt);
+		puts("------------Ciphertext Encryption------------");
+		printCharacters(asciiCharacters);
+		puts("---------------------------------------------");
+		printCharacters(asciiEncrypt);
 
-	out_file_encryption = fopen("huckleberry_finn_encrypt.txt", "w");
-	if(out_file_encryption == NULL)
-	{
-		puts("ERROR: Cannot write to the encrypted file!");
-		return -3;
+		out_file_encryption = fopen("huckleberry_finn_encrypt.txt", "w");
+		if(out_file_encryption == NULL)
+		{
+			perror("ERROR: Cannot write to the encrypted file!");
+			exit(EXIT_FAILURE);
+		}
+
+		encryptMessage(buffer, size);
+
+		for(i = 0; i < size; i++)
+		{
+			fputc(*(buffer + i), out_file_encryption);
+		}
+		fclose(out_file_encryption);
+		puts("The Huckleberry Finn file has been encrypted!");
+
+		in_file_decryption = fopen(argv[2], "r");
+
+		if(in_file_decryption == NULL)
+        	{
+                	perror("ERROR: File decryption cannot be opened!");
+                	exit(EXIT_FAILURE);
+        	}
+        	fseek(in_file_decryption, 0, SEEK_END);
+        	size = ftell(in_file_decryption);
+        	rewind(in_file_decryption);
+        	char description[size];
+        	i = 0;
+        	while((read = fgetc(in_file_decryption)) != EOF)
+        	{
+                	*(description + i) = read;
+                	i++;
+        	}
+        	fclose(in_file_decryption);
+
+		decryptCharacters(asciiEncrypt, asciiDecrypt);
+		printf("\n");
+		puts("------------Ciphertext Decryption------------");
+        	printCharacters(asciiEncrypt);
+		puts("---------------------------------------------");
+		printCharacters(asciiDecrypt);
+
+		out_file_decryption = fopen("hamlet_decrypt.txt", "w");
+        	if(out_file_decryption == NULL)
+        	{
+                	perror("ERROR: Cannot write to the decrypted file!");
+                	exit(EXIT_FAILURE);
+        	}
+
+        	decryptMessage(description, size);
+
+        	for(i = 0; i < size; i++)
+        	{
+                	fputc(*(description + i), out_file_decryption);
+        	}
+        	fclose(out_file_decryption);
+        	puts("The Hamlet file has been decrypted!");
 	}
-
-	encryptMessage(buffer, size);
-
-	for(i = 0; i < size; i++)
-	{
-		fputc(buffer[i], out_file_encryption);
-	}
-	fclose(out_file_encryption);
-	puts("The Huckleberry Finn file has been encrypted!");
-
-	in_file_decryption = fopen("hamlet_encrypt.txt", "r");
-
-	if(in_file_decryption == NULL)
-        {
-                puts("ERROR: File decryption cannot be opened!");
-                return -4;
-        }
-        fseek(in_file_decryption, 0, SEEK_END);
-        size = ftell(in_file_decryption);
-        rewind(in_file_decryption);
-        char description[size];
-        i = 0;
-        while((read = fgetc(in_file_decryption)) != EOF)
-        {
-                description[i] = read;
-                i++;
-        }
-        fclose(in_file_decryption);
-
-	decryptAlphabet(alphabetEncrypt, alphabetDecrypt);
-	printf("\n");
-	puts("------------Ciphertext Decryption------------");
-        printAlphabet(alphabetEncrypt);
-	printAlphabet(alphabetDecrypt);
-
-	out_file_decryption = fopen("hamlet_decrypt.txt", "w");
-        if(out_file_decryption == NULL)
-        {
-                puts("ERROR: Cannot write to the decrypted file!");
-                return -5;
-        }
-
-        decryptMessage(description, size);
-
-        for(i = 0; i < size; i++)
-        {
-                fputc(description[i], out_file_decryption);
-        }
-        fclose(out_file_decryption);
-        puts("The Hamlet file has been decrypted!");
 	return 0;
 }
 
-void storeAlphabet(char alphabet[])
+void storeCharacters(char characters[])
 {
-	char letter = 65;
-        for(int i = 0; i < 26; i++)
+	char asciiCode = 33;
+        for(int i = 0; i < 94; i++)
         {
-                alphabet[i] = letter;
-                letter = letter + 1;
+                characters[i] = asciiCode;
+                asciiCode = asciiCode + 1;
         }
 }
 
-void encryptAlphabet(char alphabet[], char encrypt[])
+void encryptCharacters(char characters[], char encrypt[])
 {
-        int start = 65;
-        for(int i = 0; i < 26; i++)
+        int start = 33;
+        for(int i = 0; i < 94; i++)
         {
-                encrypt[i] = start + ((3 * alphabet[i] + 8) % 26);
+                encrypt[i] = start + (5 * (characters[i] - start) % 94);
         }
 }
 
-void decryptAlphabet(char encrypt[], char decrypt[])
+void decryptCharacters(char encrypt[], char decrypt[])
 {
-	int start = 65;
-        for(int i = 0; i < 26; i++)
+	int start = 33;
+        for(int i = 0; i < 94; i++)
         {
-                decrypt[i] = start + (((encrypt[i] - 8) * 9) % 26);
+                decrypt[i] = start + (((encrypt[i] - start) * 19) % 94);
         }
 }
 
-void printAlphabet(char alphabet[])
+void printCharacters(char characters[])
 {
-	for(int i = 0; i < 26; i++)
+	for(int i = 0; i < 94; i++)
         {
-                printf("%c", alphabet[i]);
-                if(i != 25)
+                printf("%c", characters[i]);
+                if(i != 93)
                 {
                         printf(" ");
                 }
@@ -152,42 +158,26 @@ void printAlphabet(char alphabet[])
         printf("\n");
 }
 
-void encryptMessage(char buffer[], int size)
+void encryptMessage(char* buffer, int size)
 {
+	int start = 33;
 	for(int i = 0; i < size; i++)
         {
-                if((buffer[i] >= 65 && buffer[i] <= 90) || (buffer[i] >= 97 && buffer[i] <= 122))
+                if(*(buffer + i) >= 33 && *(buffer + i) <= 126)
                 {
-			int start = 65;
-			if(islower(buffer[i]))
-			{
-				buffer[i] = toupper(buffer[i]);
-				buffer[i] = tolower(start + ((3 * buffer[i] + 8) % 26));
-			}
-			else
-			{
-                                buffer[i] = start + ((3 * buffer[i] + 8) % 26);
-			}
+                        *(buffer + i) = start + ((5 * (*(buffer + i) - start)) % 94);
                 }
 	}
 }
 
-void decryptMessage(char description[], int size)
+void decryptMessage(char* description, int size)
 {
+	int start = 33;
         for(int i = 0; i < size; i++)
         {
-                if((description[i] >= 65 && description[i] <= 90) || (description[i] >= 97 && description[i] <= 122))
+                if(*(description + i) >= 33 && *(description + i) <= 126)
                 {
-                        int start = 65;
-			if(islower(description[i]))
-			{
-				description[i] = toupper(description[i]);
-				description[i] = tolower(start + (((description[i] - 8) * 9) % 26));
-			}
-			else
-			{
-				description[i] = start + (((description[i] - 8) * 9) % 26);
-			}
+			*(description + i) = start + (((*(description + i) - start) * 19) % 94);
 		}
 	}
 }
